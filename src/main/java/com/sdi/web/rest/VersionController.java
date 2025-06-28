52063f24090ba7563d19067a24c4d1d3138c5137
ll-laila <lailatimasli1@example.com>
2025-06-28 17:12:29 +0100

    handle version

--- src/main/java/com/sdi/web/rest/VersionController.java
+++ src/main/java/com/sdi/web/rest/VersionController.java
@@ -2,26 +2,17 @@ package com.sdi.web.rest;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
-
 public class VersionController {
 
-    @Value("${git.commit.id.describe:unknown}")
-    private String gitDescribe;
+    @Value("${project.version:Unknown}")
+    private String projectVersion;
 
-    @GetMapping("/version")
+    @GetMapping("/Backend-Version")
     public ResponseEntity<String> getVersion() {
-        if (gitDescribe == null || gitDescribe.isEmpty()) {
-            return ResponseEntity.ok("unknown");
-        }
-        // extraire le tag, la partie avant le premier '-'
-        String tag = gitDescribe.split("-")[0];
-        return ResponseEntity.ok(tag);
+        return ResponseEntity.ok(projectVersion);
     }
-
 }
